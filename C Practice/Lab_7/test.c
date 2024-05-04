2f125e766950585b91422e7e5a965b9cb12bee5a
Thunboo <broydogl@gmail.com>
2024-05-05 00:47:09 +0300

    Some progress with 7th lab in C
    There is still some features to add (commented in code)
    
    Also have to make it to write it into .txt file. But soon it'll be done)

--- C Practice/Lab_7/test.c
+++ C Practice/Lab_7/test.c
@@ -5,6 +5,12 @@
 #include <locale.h>
 #include "RU_CL_console.h"
 
+// Добавить генерацию отчеств       => malloc
+// Добавить генерацию даты рождения => malloc
+// Добавить проверку возраста
+// Добавить поиск по отчеству
+// Дополнить сортировку изменением отчества и даты рождения
+
 #define color_output_blue  SetConsoleTextAttribute(hConsole, 3);
 #define color_output_green SetConsoleTextAttribute(hConsole, 2);
 #define color_output_red   SetConsoleTextAttribute(hConsole, 4);
@@ -38,27 +44,10 @@ char * months[12] = {"Jan", "Feb", "Mar", "Apr",
 
 time_t rawtime;
 struct tm * timeinfo;
-/*
-int main(){
-    FILE *fptr;
-    if ((fptr = fopen("Text.txt", "w+")) == NULL){
-        perror("Error while opening the File");
-    }
-
-    fclose(fptr);
-    return 0;
-}
-*/
 
 struct student *students;
 int current_size = SIZE_OF_DATABASE, max_size = SIZE_OF_DATABASE * 2;
-int digit_counter(char* sentence){
-    int cnt = 0;
-    for (int i = 0; i < strlen(sentence); i++){
-        if (isdigit(sentence[i])) cnt++;
-    }
-    return cnt;
-}
+
 void main_menu(){
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     SEPARATOR
@@ -150,45 +139,7 @@ void print_database(){
 void clear_memory(){
     free(students);
 }
-void add(){
-    struct student new_student;
-    char new_name[24], new_surname[24];
-    getchar();
-    printf("Введите:\nФамилию > ");  gets(new_surname); new_student.FIO.surname = (char *) malloc(sizeof(new_surname) + 1); strcpy(new_student.FIO.surname, new_surname); //new_student.surname = new_surname;
-    printf("Имя > ");                gets(new_name);    new_student.FIO.name    = (char *) malloc(sizeof(new_name)    + 1); strcpy(new_student.FIO.name,    new_name);    //new_student.name    = new_name;
-    printf("Номер группы > ");       scanf("%d", &new_student.group);
-    if (current_size == max_size){
-        max_size *= 2;
-        students = realloc(students, max_size * sizeof(struct student));
-    }
-    students[current_size] = new_student;
-    current_size++;
-}
-void sort_database(){
-    struct student temp;
-    for (int i = 0; i < current_size; i++){
-        for (int j = 0; j < current_size; j++){
-            if (strcmp(students[i].FIO.surname, students[j].FIO.surname) < 0){
-                temp.FIO.surname = (char *) malloc(sizeof(students[i].FIO.surname) + 1);
-                strcpy(temp.FIO.surname, students[i].FIO.surname);
-                temp.FIO.name = (char *) malloc(sizeof(students[i].FIO.name) + 1);
-                strcpy(temp.FIO.name,    students[i].FIO.name);
-                temp.group = students[i].group;
 
-                strcpy(students[i].FIO.surname, students[j].FIO.surname);
-                strcpy(students[i].FIO.name,    students[j].FIO.name);
-                students[i].group = students[j].group;
-
-                strcpy(students[j].FIO.surname, temp.FIO.surname);
-                strcpy(students[j].FIO.name, temp.FIO.name);
-                students[j].group = temp.group;
-            
-                free(temp.FIO.surname);
-                free(temp.FIO.name);
-            }
-        }
-    }
-}
 void find_from_group(int* found_cnt){
     CLEAR_
     int group_;
@@ -197,144 +148,171 @@ void find_from_group(int* found_cnt){
     printf("Студенты группы №%d :\n", group_); SEPARATOR
     for (int i = 0; i < current_size; i++){
         if (students[i].group == group_){
-            *found_cnt++;
+            *found_cnt += 1;
             printf("| %14s | %12s |\n", students[i].FIO.surname, students[i].FIO.name);
         }
     }
-    SEPARATOR PAUSE_ CLEAR_
+    if (*found_cnt != 0) {SEPARATOR PAUSE_ CLEAR_}
 }
-void search_(){
+void find_by_surname(int* found_cnt){
     CLEAR_
-    char selection = '0';
-    char FIO_search = '0';
-    char name_[24], surname_[24];
-    int found_cnt = 0, age = 0;
-    while ((selection != '2') && (selection != '3') && (selection != '4')){
-        printf("Введите тип поиска:\n-------------------\n");
-        printf("1. По ФИО\n2. По возрасту (> n)\n3. По номеру группы №\n4. Выйти в меню");
-        printf("\n-------------------\n");
-        printf("\nSTATE 01\n");
-        selection = getchar();
-        printf("\nSTATE 02\n");
-        // CLEAR_ 
-        printf("\nSTATE 03\n");
-        selection = getchar();
-        
-        if (selection == '1'){
-            while ((FIO_search != '1') && (FIO_search != '2') && (FIO_search != '3') && (FIO_search != '4') && (FIO_search != '5')){
-                printf("Уточните тип поиска:\n-------------------\n");
-                printf("1. По фамилии\n2. По имени\n3. По фамилии и имени\n4.По отчеству\n5. Выйти в меню поиска");
-                printf("\n-------------------\n");
-                printf("\nSTATE 1\n");
-                getchar();
-                printf("\nSTATE 2\n"); 
-                FIO_search = getchar();
-                printf("\nSTATE 3\n");
-                // CLEAR_
-                printf("\nSTATE 4\n");
+    char surname_buffer[24];
+    getchar();
+    printf("Введите Фамилию > ");    gets(surname_buffer); CLEAR_
+
+    printf("Найденные студенты с фамилией %14s :\n", surname_buffer); SEPARATOR
+    for (int i = 0; i < current_size; i++){
+        if (strstr(surname_buffer, students[i].FIO.surname)){
+            if(strlen(surname_buffer) == strlen(students[i].FIO.surname)){ 
+                *found_cnt += 1;
+                printf("| %14s | %12s | ГРУППА № %2d |\n", students[i].FIO.surname, students[i].FIO.name, students[i].group);
             }
         }
+    } 
+    if (*found_cnt != 0) {SEPARATOR PAUSE_ CLEAR_}
+}
+void find_by_name(int* found_cnt){
+    CLEAR_
+    char name_buffer[24];
+    getchar();
+    printf("Введите Имя > ");        gets(name_buffer); CLEAR_
 
-        if (FIO_search != '5') { break; }
-    }
+    printf("Найденные студенты с именем %12s :\n", name_buffer); SEPARATOR
+    for (int i = 0; i < current_size; i++){
+        if (strstr(name_buffer, students[i].FIO.name)){
+            if(strlen(name_buffer) == strlen(students[i].FIO.name)){ 
+                *found_cnt += 1;
+                printf("| %14s | %12s | ГРУППА № %2d |\n", students[i].FIO.surname, students[i].FIO.name, students[i].group);
+            }
+        }
+    } 
+    if (*found_cnt != 0) {SEPARATOR PAUSE_ CLEAR_}
+}
+void find_by_both(int* found_cnt){
+    CLEAR_
+    char name_buffer[24], surname_buffer[24];
+    getchar();
+    printf("Введите:\nФамилию > ");  gets(surname_buffer);
+    printf("\nИмя > ");              gets(name_buffer);    CLEAR_
 
-    // getchar();
-    if (selection == '1'){
-        printf("got onto selection == '1'");
-        switch(FIO_search){
-            printf("got onto switch fio_search");
-            case '1':
-                printf("Введите Фамилию > ");    gets(surname_); //CLEAR_
-
-                printf("Найденные студенты с фамилией %14s :\n", surname_); SEPARATOR
-                for (int i = 0; i < current_size; i++){
-                    if (strstr(surname_, students[i].FIO.surname)){
-                        if(strlen(surname_) == strlen(students[i].FIO.surname)){ found_cnt++;
-                            printf("| %14s | %12s | ГРУППА № %2d |\n", students[i].FIO.surname, students[i].FIO.name, students[i].group);
-                        }
+    printf("Найденные студенты с фамилией %14s и именем %12s :\n", surname_buffer, name_buffer); SEPARATOR
+    for (int i = 0; i < current_size; i++){
+        if (strstr(surname_buffer, students[i].FIO.surname)){
+            if(strlen(surname_buffer) == strlen(students[i].FIO.surname)){ 
+                if (strstr(name_buffer, students[i].FIO.name)){
+                    if(strlen(name_buffer) == strlen(students[i].FIO.name)){ 
+                        *found_cnt += 1;
+                        printf("| %14s | %12s | ГРУППА № %2d |\n", students[i].FIO.surname, students[i].FIO.name, students[i].group);
                     }
-                } 
+                }
+            }
+        }
+    } 
+    if (*found_cnt != 0) {SEPARATOR PAUSE_ CLEAR_}
+}
+void find_by_fname(int* found_cnt){
+    CLEAR_
+    char fname_buffer[24];
+    getchar();
+    printf("Введите Отчество > ");        gets(fname_buffer); CLEAR_
+
+    printf("Найденные студенты с отчеством %12s :\n", fname_buffer); SEPARATOR
+    for (int i = 0; i < current_size; i++){
+        if (strstr(fname_buffer, students[i].FIO.fname)){
+            if(strlen(fname_buffer) == strlen(students[i].FIO.fname)){ 
+                *found_cnt += 1;
+                printf("| %14s | %12s | %12s | ГРУППА № %2d |\n", students[i].FIO.surname, students[i].FIO.name, students[i].FIO.fname, students[i].group);
+            }
+        }
+    } 
+    if (*found_cnt != 0) SEPARATOR PAUSE_ CLEAR_
+}
+void search2(){
+    int inMenu1 = 1, inMenu2 = 1;
+    char Menu_Option_1 = '0', Menu_Option_2 = '0';
+    int found_cnt = 0, age = 0;
+
+    while (inMenu1){
+        CLEAR_
+        SEPARATOR
+        printf("Choose search type:\n");
+        printf("1. FIO\n2. Age\n3. Group\n4. Back to menu\n");
+        Menu_Option_1 = getchar();
+
+        switch(Menu_Option_1){
+            case '1':
+                inMenu2 = 1;
+                while (inMenu2){
+                    CLEAR_
+                    SEPARATOR
+                    printf("Specify seatch type:\n");
+                    printf("1. Surname\n2. Name\n3. Both\n4. Other\n5. Back to menu\n");
+                    Menu_Option_2 = getchar();
+                    
+                    switch(Menu_Option_2){
+                        case '1':
+                            find_by_surname(&found_cnt);
+                            inMenu1 = 0; inMenu2 = 0;
+                            break;
+                        case '2':
+                            find_by_name(&found_cnt);
+                            inMenu1 = 0; inMenu2 = 0;
+                            break;
+                        case '3':
+                            find_by_both(&found_cnt);
+                            inMenu1 = 0; inMenu2 = 0;
+                            break;
+                        case '4':
+                            find_by_fname(&found_cnt);
+                            inMenu1 = 0; inMenu2 = 0;
+                            break;
+                        case '5':
+                            inMenu2 = 0;
+                            break;
+                    }
+                }
                 break;
             case '2':
-                printf("Введите Имя > ");        gets(name_); //CLEAR_
-
-                printf("Найденные студенты с именем %12s :\n", name_); SEPARATOR
-                for (int i = 0; i < current_size; i++){
-                    if (strstr(name_, students[i].FIO.name)){
-                        if(strlen(name_) == strlen(students[i].FIO.name)){ found_cnt++;
-                            printf("| %14s | %12s | ГРУППА № %2d |\n", students[i].FIO.surname, students[i].FIO.name, students[i].group);
-                        }
-                    }
-                } 
+                printf("age search...\n"); PAUSE_
+                inMenu1 = 0;
                 break;
             case '3':
-                printf("Введите:\nФамилию > ");  gets(surname_);
-                printf("\nИмя > ");              gets(name_); //CLEAR_
-
-                printf("Найденные студенты с фамилией %14s и именем %12s :\n", surname_, name_); SEPARATOR
-                for (int i = 0; i < current_size; i++){
-                    if (strstr(surname_, students[i].FIO.surname)){
-                        if(strlen(surname_) == strlen(students[i].FIO.surname)){ 
-                            if (strstr(name_, students[i].FIO.name)){
-                                if(strlen(name_) == strlen(students[i].FIO.name)){ found_cnt++;
-                                    printf("| %14s | %12s | ГРУППА № %2d |\n", students[i].FIO.surname, students[i].FIO.name, students[i].group);
-                                }
-                            }
-                        }
-                    }
-                } 
+                find_from_group(&found_cnt);
+                inMenu1 = 0;
                 break;
             case '4':
-                printf("\nUNDER CONSTRUCTION\n"); PAUSE_
+                inMenu1 = 0;
                 break;
-            case '5':
+            default:
                 break;
         }
-    }
-    if (selection == '2'){
-        printf("got onto selection == '2'");
-        while (age <= 0){
-            printf("Введите Возраст > ");   gets(surname_); CLEAR_
-            if (digit_counter(surname_) == strlen(surname_)){
-                age = atoi(surname_);
-            }
-
-            time(&rawtime);
-            timeinfo = localtime(&rawtime);
-            printf("Время сейчас : %d.%d.%d - %d:%d:%d\n", timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec, timeinfo->tm_mday, timeinfo->tm_mon, timeinfo->tm_year + 1900);
-
-            printf("Найденные студенты старше %d :\n", age); SEPARATOR
-            for (int i = 0; i < current_size; i++){
-                if (students[i].birthdate.year > timeinfo->tm_year - age){
-                    continue;
-                }
-                else if (students[i].birthdate.month > timeinfo->tm_mon){
-                    continue;
-                }
-                else if (students[i].birthdate.day > timeinfo->tm_mday){
-                    continue;
-                }
-                else{
-                    found_cnt++;
-                    printf("| %14s | %12s | ГРУППА № %2d | %d.%d.%d\n", 
-                            students[i].FIO.surname,   students[i].FIO.name,        students[i].group, 
-                            students[i].birthdate.day, students[i].birthdate.month, students[i].birthdate.year);
-                }
-                
-            } 
+        
+        int was_finding = 0;
+        switch(Menu_Option_1){
+            case '2':
+            case '3':
+                was_finding = 1;
+                break;
+            default:
+                break;
+        }
+        switch(Menu_Option_2){
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+                was_finding = 1;
+                break;
+            default:
+                break;
+        }
+        if (was_finding == 1 && found_cnt == 0){
+            printf("Не было найдено студентов по такому запросу... \n\n");
+            PAUSE_
         }
     }
-    if (selection == '3'){
-        printf("got onto selection == '3'");
-        find_from_group(&found_cnt);
-    }
-    if (selection == '1' || '2'){
-        if (found_cnt > 0) SEPARATOR
-        else printf("Не было найдено студентов по такому запросу... \n\n");
-    }
-    // if (selection != '4') PAUSE_ // ?
-    CLEAR_
 }
+
 int main(){
     time_t CT; srand(time(&CT));  
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -342,7 +320,6 @@ int main(){
 
     students = (struct student*) malloc(max_size * sizeof(struct student));
     form_database();
-    
     int option = -1, MENU = 1;
 
     while (MENU){
@@ -350,22 +327,11 @@ int main(){
         main_menu(option);
         scanf("%d", &option); color_output_white
         switch (option){
-            case 1:
-                add();
-                break;
             case 2:
                 print_database();
                 break;
             case 3:
-                search_();
-                break;
-            case 4:
-                //find_from_group();
-                CLEAR_
-                printf("NOTHING HERE\n"); PAUSE_
-                break;
-            case 5:
-                sort_database();
+                search2();
                 break;
             case 6:
                 MENU = 0;
@@ -374,7 +340,6 @@ int main(){
                 printf("ЗАКРЫВАЕМ ПРОГРАММУ."); sleep(1); printf("."); sleep(1); printf("."); sleep(1);
                 color_output_white
                 break;
-            case 11: printf("%d\n", current_size); PAUSE_ break;
             default:
                 break;
         }   
