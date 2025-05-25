fa06d74291511e3fdc1ad2ac97e64672426fc06f
Thunboo <broydogl@gmail.com>
2025-05-25 13:24:44 +0300

    Finished 20 Queries for DB Project
    - Remade the create.sql file - restructurized the DB itself
    - Added new constraints to DB tables
    - Changed the insert.sql files so that it matches the new structure of DB
    - Composed 20 Queries for DB

--- 4th Term/DataBases/Project/create.sql
+++ 4th Term/DataBases/Project/create.sql
@@ -3,14 +3,9 @@ CREATE TABLE school_cars (
     car_plate varchar(9) NOT NULL,
 	brand varchar(30) NOT NULL,
 	model varchar(30) NOT NULL,
-    manufacture_year int,
-	CONSTRAINT manufacture_year_check CHECK (manufacture_year > 1990 AND manufacture_year <= EXTRACT(year from CURRENT_DATE)::int)
+    manufacture_year int
 );
-ALTER TABLE ONLY school_cars
-    ADD CONSTRAINT school_cars_pkey PRIMARY KEY (car_plate);
-ALTER TABLE ONLY school_cars
-	ADD CONSTRAINT school_cars_car_plate_check CHECK (car_plate ~* '^[АВЕКМНОРСТУХ]{1}[0-9]{3}[АВЕКМНОРСТУХ]{2}[0-9]{2,3}$');
- 
+
 -- Создаём таблицу для выпускников Автошколы
 CREATE TABLE graduates (
     graduate_certificate_id varchar(20),
@@ -19,11 +14,6 @@ CREATE TABLE graduates (
 	patronymic varchar(30),
 	graduation_date date NOT NULL
 );
-ALTER TABLE ONLY graduates
-    ADD CONSTRAINT graduates_pkey PRIMARY KEY (graduate_certificate_id);
-ALTER TABLE ONLY graduates
-	ADD CONSTRAINT graduates_graduation_date_check CHECK (graduation_date < CURRENT_DATE + '1 days'::interval);
-CREATE SEQUENCE graduates_certificate_seq;
 	
 -- Создаём таблицу Преподавателей и Инструкторов
 CREATE TABLE professors (
@@ -31,11 +21,8 @@ CREATE TABLE professors (
 	last_name varchar(30) NOT NULL,
 	first_name varchar(30) NOT NULL,
 	patronymic varchar(30),
-    experience_years int NOT NULL,
-	CONSTRAINT professors_experience_check CHECK (experience_years > 3)
+    experience_years int NOT NULL
 );
-ALTER TABLE ONLY professors
-    ADD CONSTRAINT professors_pkey PRIMARY KEY (professor_id);
 
 -- Создаём таблицу трудоустроств Преподавателей и Инструкторов
 CREATE TABLE employments (
@@ -43,21 +30,13 @@ CREATE TABLE employments (
     employed_since date,
 	salary money
 );
-ALTER TABLE ONLY employments
-    ADD CONSTRAINT employments_pkey PRIMARY KEY (professor_id);
-ALTER TABLE ONLY employments
-    ADD CONSTRAINT employments_professor_id_fkey FOREIGN KEY (professor_id) REFERENCES professors(professor_id) ON DELETE CASCADE;
-ALTER TABLE ONLY employments
-	ADD CONSTRAINT employments_employed_since_check CHECK (employed_since < CURRENT_DATE + '15 days'::interval)
 
 -- Создаём таблицу под группы учащихся
 CREATE TABLE students_groups (
-    group_id int NOT NULL,
+    group_id int, --NOT NULL,
     enrollment_date date DEFAULT CURRENT_DATE NOT NULL,
 	graduation_date date DEFAULT (CURRENT_DATE + '3 months'::interval) NOT NULL
 );
-ALTER TABLE ONLY students_groups
-    ADD CONSTRAINT students_groups_pkey PRIMARY KEY (group_id);
 	
 -- Создаём таблицу Учащихся
 CREATE TABLE students (
@@ -65,38 +44,80 @@ CREATE TABLE students (
 	last_name varchar(30) NOT NULL,
 	first_name varchar(30) NOT NULL,
 	patronymic varchar(30),
-	group_id int NOT NULL,
+	group_id int DEFAULT NULL,
+	car_plate varchar(9) NOT NULL DEFAULT 'О000ОО00',
 	email varchar(30),
-	inn varchar(12) DEFAULT '770000000000'::character varying,
-    CONSTRAINT email_check CHECK (((email)::text ~* '^[A-Za-z0-9._+%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$'::text))
+	inn varchar(12) DEFAULT '770000000000'::character varying
 );
-ALTER TABLE ONLY students
-    ADD CONSTRAINT students_pkey PRIMARY KEY (student_id);
-ALTER TABLE ONLY students
-    ADD CONSTRAINT students_students_groups_fkey FOREIGN KEY (group_id) REFERENCES students_groups(group_id) ON DELETE CASCADE;
-ALTER TABLE ONLY students
-	ADD COLUMN IF NOT EXISTS car_plate varchar(9) NOT NULL DEFAULT 'О000ОО00';
-ALTER TABLE ONLY students
-	ADD CONSTRAINT students_cars_fkey FOREIGN KEY (car_plate) REFERENCES school_cars(car_plate) ON DELETE SET DEFAULT;
 
 -- Создаём таблицу занятий (связь профессор-студент)
 CREATE TABLE lessons (
-    lesson_id SERIAL,
 	lesson_type varchar(8),
-    professor_id int,
-    student_id int,
+    professor_id int NOT NULL,
+    student_id int NOT NULL,
 	group_id int,
-	CONSTRAINT lessons_group_or_student CHECK ((student_id IS NOT NULL) OR (group_id IS NOT NULL)),
-	CONSTRAINT lesson_type_check CHECK (lesson_type ~* '^Практика$' OR lesson_type ~* '^Теория$')
+	lesson_date date DEFAULT CURRENT_DATE NOT NULL
 );
+
+-- Ограничения -------------------------------
+
+-- school_cars
+ALTER TABLE ONLY school_cars
+    ADD CONSTRAINT school_cars_pkey PRIMARY KEY (car_plate);
+ALTER TABLE ONLY school_cars
+	ADD CONSTRAINT school_cars_car_plate_check CHECK (car_plate ~* '^[АВЕКМНОРСТУХ]{1}[0-9]{3}[АВЕКМНОРСТУХ]{2}[0-9]{2,3}$');
+ALTER TABLE ONLY school_cars
+	ADD CONSTRAINT school_cars_manufacture_year_check 
+		CHECK (manufacture_year > 1990 AND manufacture_year <= EXTRACT(year from CURRENT_DATE)::int);
+ 
+-- graduates
+ALTER TABLE ONLY graduates
+    ADD CONSTRAINT graduates_pkey PRIMARY KEY (graduate_certificate_id);
+ALTER TABLE ONLY graduates
+	ADD CONSTRAINT graduates_graduation_date_check CHECK (graduation_date < CURRENT_DATE + '1 days'::interval);
+CREATE SEQUENCE graduates_certificate_seq;
+
+-- professors
+ALTER TABLE ONLY professors
+    ADD CONSTRAINT professors_pkey PRIMARY KEY (professor_id);
+ALTER TABLE ONLY professors
+	ADD CONSTRAINT professors_experience_check CHECK (experience_years > 3);
+
+-- employments
+ALTER TABLE ONLY employments
+    ADD CONSTRAINT employments_pkey PRIMARY KEY (professor_id);
+ALTER TABLE ONLY employments
+    ADD CONSTRAINT employments_professor_id_fkey FOREIGN KEY (professor_id) REFERENCES professors(professor_id) ON DELETE CASCADE;
+ALTER TABLE ONLY employments
+	ADD CONSTRAINT employments_employed_since_check CHECK (employed_since < CURRENT_DATE + '15 days'::interval);
+
+-- students_groups
+ALTER TABLE ONLY students_groups
+    ADD CONSTRAINT students_groups_pkey PRIMARY KEY (group_id);
+
+-- students
+ALTER TABLE ONLY students
+    ADD CONSTRAINT students_pkey PRIMARY KEY (student_id);
+ALTER TABLE ONLY students
+    ADD CONSTRAINT students_students_groups_fkey FOREIGN KEY (group_id) REFERENCES students_groups(group_id) ON DELETE SET DEFAULT;
+ALTER TABLE ONLY students
+	ADD CONSTRAINT students_cars_fkey FOREIGN KEY (car_plate) REFERENCES school_cars(car_plate) ON DELETE SET DEFAULT;
+ALTER TABLE ONLY students
+    ADD CONSTRAINT students_email_check CHECK ((email)::text ~* '^[A-Za-z0-9._+%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$'::text);
+
+-- lessons
 ALTER TABLE ONLY lessons
-    ADD CONSTRAINT lessons_pkey PRIMARY KEY (lesson_id);
+    ADD CONSTRAINT lessons_pkey PRIMARY KEY (student_id, group_id, lesson_date);
 ALTER TABLE ONLY lessons
-    ADD CONSTRAINT lessons_professor_id_fkey FOREIGN KEY (professor_id) REFERENCES professors(professor_id) ON DELETE CASCADE;
+    ADD CONSTRAINT lessons_professor_id_fkey FOREIGN KEY (professor_id) REFERENCES professors(professor_id);-- ON DELETE CASCADE;
 ALTER TABLE ONLY lessons
-    ADD CONSTRAINT lessons_student_id_fkey FOREIGN KEY (student_id) REFERENCES students(student_id) ON DELETE CASCADE;
+    ADD CONSTRAINT lessons_student_id_fkey FOREIGN KEY (student_id) REFERENCES students(student_id);-- ON DELETE CASCADE;
 ALTER TABLE ONLY lessons
-    ADD CONSTRAINT lessons_group_id_fkey FOREIGN KEY (group_id) REFERENCES students_groups(group_id) ON DELETE CASCADE;
+    ADD CONSTRAINT lessons_group_id_fkey FOREIGN KEY (group_id) REFERENCES students_groups(group_id);-- ON DELETE CASCADE;
+ALTER TABLE ONLY lessons
+	ADD CONSTRAINT lesson_type_check CHECK (lesson_type ~* '^Практика$' OR lesson_type ~* '^Теория$');
+	
+-- Триггеры, процедуры и функции ----------------------
 
 CREATE OR REPLACE FUNCTION compute_certificate_id()
 RETURNS TRIGGER
@@ -104,10 +125,11 @@ LANGUAGE PLPGSQL AS
 $$
 BEGIN
 	IF NEW.graduation_date IS NULL
-		THEN NEW.graduation_date = CURRENT_DATE; end if;
+		THEN NEW.graduation_date = CURRENT_DATE; 
+	END IF;
  	NEW.graduate_certificate_id := '777-' ||
-									CURRENT_DATE || '-' || 
-									LPAD(nextval('graduates_certificate_seq')::text, 5, '0');
+		CURRENT_DATE || '-' || 
+		LPAD(nextval('graduates_certificate_seq')::text, 5, '0');
  	RETURN NEW;
 END;
 $$;
@@ -117,3 +139,67 @@ BEFORE INSERT
 ON graduates
 FOR EACH ROW
 EXECUTE FUNCTION compute_certificate_id();
+
+----------------
+CREATE OR REPLACE FUNCTION no_group_id()
+RETURNS TRIGGER
+LANGUAGE PLPGSQL AS
+$$
+BEGIN
+	IF NEW.group_id IS NULL
+		THEN NEW.group_id = -1; 
+	END IF;
+ 	RETURN NEW;
+END;
+$$;
+
+CREATE OR REPLACE TRIGGER no_group_id_given 
+BEFORE INSERT
+ON students_groups
+FOR EACH ROW
+EXECUTE FUNCTION no_group_id();
+
+CREATE OR REPLACE TRIGGER no_group_id_given 
+BEFORE INSERT
+ON lessons
+FOR EACH ROW
+EXECUTE FUNCTION no_group_id();
+----------------
+CREATE OR REPLACE PROCEDURE no_empty_group_id()
+LANGUAGE PLPGSQL AS
+$$
+BEGIN
+	IF (SELECT count(*) FROM students_groups
+		WHERE group_id = -1) = 0
+		THEN INSERT INTO students_groups(group_id, enrollment_date, graduation_date)
+		VALUES (-1, '01-01-2023', '01-01-2023'); 
+	END IF;
+END;
+$$;
+
+CALL no_empty_group_id();
+
+CREATE OR REPLACE PROCEDURE delete_groups_except_empty()
+LANGUAGE PLPGSQL AS
+$$
+BEGIN
+	DELETE FROM students_groups;
+	CALL no_empty_group_id(); 
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION dont_remove_empty_group_id_func() 
+RETURNS TRIGGER
+LANGUAGE PLPGSQL AS
+$$
+BEGIN
+	CALL delete_groups_except_empty();
+	RETURN NEW;
+END;
+$$;
+
+CREATE OR REPLACE TRIGGER dont_remove_empty_group_id 
+AFTER DELETE
+ON students_groups
+FOR EACH ROW
+EXECUTE FUNCTION dont_remove_empty_group_id_func();
--- 4th Term/DataBases/Project/queries.sql
+++ 4th Term/DataBases/Project/queries.sql
@@ -1,3 +1,11 @@
+SELECT * FROM employments;
+SELECT * FROM graduates;
+SELECT * FROM lessons;
+SELECT * FROM professors;
+SELECT * FROM school_cars;
+SELECT * FROM students;
+SELECT * FROM students_groups;
+
 /*
  	№1
   	Выбрать ФИО всех студенток из всех нечетных групп, зачисленных после 31.12.2024
@@ -102,3 +110,169 @@ ORDER BY 4 DESC, 5, 1, 2, 3;
 SELECT DISTINCT lesson_id, lesson_type, professor_id, sg.group_id, enrollment_date
 FROM lessons l
 INNER JOIN students_groups sg ON sg.group_id = l.group_id;
+
+/*
+	№9
+	Вывести всех преподавателей с ЗП выше средней по Автошколе.
+	Первой строкой также вывести среднюю ЗП.
+*/
+select last_name, first_name, patronymic, salary
+from (
+(
+	SELECT last_name, first_name, patronymic, salary, t
+	FROM professors p
+	JOIN employments e ON p.professor_id = e.professor_id
+	CROSS JOIN (VALUES (2)) as sid (t)
+	WHERE e.salary::numeric > (SELECT AVG(salary::numeric) FROM employments)
+)
+UNION 
+(
+	SELECT last_name, first_name, patronymic, temp_emp.salary, t
+	FROM (
+		SELECT AVG(emp.salary::numeric)::money AS "salary" FROM employments emp
+	) as temp_emp (salary)
+	CROSS JOIN (VALUES (1)) as sid (t)
+	CROSS JOIN (VALUES ('-- Средняя'::varchar(30), 'Заработная'::varchar(30), 'Плата ='::varchar(30))) 
+		as f (last_name, first_name, patronymic)
+)
+) as foo
+ORDER BY t, salary ASC;
+
+/*
+	№10
+	Вывести, сколько и каких занятий провел каждый преподаватель
+*/
+SELECT DISTINCT p.professor_id, p.last_name, p.first_name, p.patronymic, lesson_type, count(lesson_date) AS "Число занятий"
+FROM professors p
+CROSS JOIN lessons l
+WHERE p.professor_id = l.professor_id
+GROUP BY 1,2,3,4,5
+ORDER BY lesson_type DESC, 1 ASC;
+
+/*
+	№11
+	Студентов, прошедших как теорию, так и практику
+*/
+SELECT last_name, first_name, patronymic
+FROM lessons l
+LEFT JOIN students s ON s.student_id = l.student_id
+GROUP BY 1,2,3
+HAVING COUNT(DISTINCT lesson_type) = 2;
+
+/*
+	№12
+	Всех выпускников, что не являются преподавателями в нашей Автошколе
+*/
+SELECT g.last_name, g.first_name, g.patronymic
+FROM graduates g
+EXCEPT --INTERSECT
+SELECT p.last_name, p.first_name, p.patronymic
+FROM professors p
+ORDER BY 1,2,3;
+
+/*
+	№13
+	Студентов, которым преподавал один и тот же профессор и теорию, и практику
+*/
+SELECT s.last_name, s.first_name, s.patronymic, professor_id
+FROM lessons l
+LEFT JOIN students s ON s.student_id = l.student_id
+GROUP BY 1,2,3,4
+HAVING count(distinct lesson_type) = 2;
+
+/*
+	№14
+	Топ 3 самых частых учебных авто у студентов
+*/
+SELECT sc.brand, sc.model, COUNT(s.student_id) AS student_count
+FROM school_cars sc
+JOIN students s ON sc.car_plate = s.car_plate
+GROUP BY sc.brand, sc.model
+ORDER BY student_count DESC
+LIMIT 3;
+
+/*
+	№15
+	Вывести преподавателей, у которых было больше 5 практических занятий за май 2025 года
+*/
+SELECT p.professor_id, p.last_name, COUNT(DISTINCT l.lesson_date) --, lesson_date
+FROM professors p
+JOIN lessons l ON p.professor_id = l.professor_id
+WHERE l.lesson_date BETWEEN '2025-05-01' AND '2025-05-31'
+  AND l.lesson_type ~* 'практика'
+GROUP BY p.professor_id, p.last_name
+HAVING COUNT(DISTINCT l.lesson_date) > 5;
+
+/*
+	№16
+	Вывести 3 студентов, у которых было большего всего практических занятий
+	и 3 с наименьшим их числом
+*/
+SELECT * FROM (
+(
+	SELECT s.last_name, s.first_name, s.patronymic, count(*)
+	FROM lessons l
+	LEFT JOIN students s ON s.student_id = l.student_id
+	WHERE lesson_type ~* 'практика'
+	GROUP BY 1,2,3
+	ORDER BY count DESC
+	LIMIT 3
+)
+UNION
+(
+	SELECT s.last_name, s.first_name, s.patronymic, count(*)
+	FROM lessons l
+	LEFT JOIN students s ON s.student_id = l.student_id
+	WHERE lesson_type ~* 'практика'
+	GROUP BY 1,2,3
+	ORDER BY count ASC
+	LIMIT 3
+) 
+) as foo
+ORDER BY count DESC;
+
+/*
+	№17
+	Вывести все группы, в которых есть иностранные студенты (нету отчеств)
+*/
+SELECT DISTINCT group_id FROM students
+WHERE patronymic IS NULL;
+
+/*
+	№18
+	Вывести дату последнего практического занятия у каждого студента и количество этих занятий
+*/
+SELECT s.last_name, s.first_name, s.patronymic,
+       MAX(l.lesson_date) AS "Дата последнего занятия",
+       COUNT(*) AS "Число занятий"
+FROM students s
+JOIN lessons l ON s.student_id = l.student_id
+WHERE lesson_type ~* 'практика'
+GROUP BY s.student_id
+ORDER  BY 1,2,3;
+
+/*
+	№19
+	Вывести id студентов, что занимались с одним и тем же преподавателем 2 или более раз
+*/
+SELECT student_id, professor_id, COUNT(lesson_date)
+FROM lessons
+GROUP BY student_id, professor_id
+HAVING COUNT(lesson_date) >= 2;
+
+/*
+	№20
+	Вывести студентов, у которых занятия вёл хотя бы 1 из 3 преподавателей
+	с самой большой ЗП
+*/
+SELECT s.last_name, s.first_name, s.patronymic
+FROM students s
+WHERE s.student_id IN (
+	SELECT DISTINCT l.student_id FROM lessons l
+	JOIN employments emp ON emp.professor_id = l.professor_id
+	WHERE l.student_id = s.student_id
+	   AND emp.salary IN (
+			SELECT salary FROM employments
+			ORDER BY salary DESC LIMIT 3
+	  	   )
+	);
